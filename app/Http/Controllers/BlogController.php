873f2664d9b53f0bcad0cf4450a3fd88685f8a4e
zeezayaa <lifiwaa@gmail.com>
2025-08-21 14:19:32 +0700

    update terbaru

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    // Menampilkan semua blog
+    public function index()
+    {
+        $blogs = Blog::all();
+        return response()->json($blogs);
+    }
+
+    // Menyimpan blog baru
+    public function store(Request $request)
+    {
+        $request->validate([
+            'judul' => 'required',
+            'penulis' => 'required',
+            'isi' => 'required',
+            'tanggal' => 'required|date'
+        ]);
+
+        $blog = Blog::create($request->all());
+        return response()->json($blog, 201);
+    }
+
+    // Menampilkan detail blog
+    public function show($id)
+    {
+        $blog = Blog::findOrFail($id);
+        return response()->json($blog);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+Route::resource('blogs', BlogController::class);
